3a9ddd4b69a543f04a1ab608fd19b0d4929cd88f
John Peart <code@johnpe.art>
2020-05-10 20:37:15 +0100

    Add additional quotes

--- quotes.js
+++ quotes.js
@@ -1,20 +1,49 @@
 var quotes = [
+
+	// Dance songs
 	["do a little dance", "make", "a little love", "get down tonight"],
+
+	// Dua Lipa
 	["Don’t show up", "Don’t", " come out", "Don’t start caring about me now"],
+
+	// Exercise songs
 	["So come on", "come on," , "come on", "Let’s get physical"],
 	["Let’s get physical", "Physical", "I wanna get physical", "Let’s get into physical"],
+
+	// Kylie songs
 	["baby, when I heard you", "For the", "first time", "I knew we were meant to be as one"],
+	["Everybody's doing a brand-new dance, now", "Come on baby", "Do the Loco-motion", "I know you'll get to like it if you give it a chance now"],
+	["So come on, come on", "Do the", "Loco-motion", "With me"],
+	
+	// The Hokey Cokey
 	["Woah, the hokey cokey", "Woah", "the hokey cokey", "Woah, the hokey cokey"],
 	["Knees bent", "Arms", "Stretched", "Ra, Ra, Ra"],
-//	["", "", "", ""],
-//	["", "", "", ""],
-//	["", "", "", ""],
-//	["", "", "", ""],
-//	["", "", "", ""],
-//	["", "", "", ""],
-//	["", "", "", ""],
-//	["", "", "", ""],
-//	["", "", "", ""],
+	["You put your left arm in", "You put your", "left arm out", "In, out, in, out"],
+	
+	// Songs about life
+	["Whether you’re a brother", "Or whether", " you’re a mother", "You’re stayin’ alive, stayin’ alive"],
+	["Life goin’ nowhere", "Somebody", "help me, yeah", "Stayin’ alive"],
+	["Life, oh life", "Oh life,", "Oh life", "Do, doot doot dooo"],
+	
+	// Food songs
+	["I've got a cake to bake", "I've got", "no clue at all", "(cep, cep, cep, cep, cep kuuku)"],
+	
+	// Cha Cha slide
+	["Right foot, let's stomp", "Left foot", "let's stomp", "Cha cha real smooth"],
+	["Slide to the left", "Slide", "to the right", "Criss cross"],
+	["To the left", "Take it", "Back now y'all", "Two hops this time"],
+	["How low can you go?", "Can you", "go down low?", "All the way to the floor?"],
+	["Reverse, reverse", "Cha cha", "now y'all", "Cha cha again"],
+	["Take it back now y'all", "Three hops", "this time", "Right foot now"],
+	["Cha cha now", "Cha cha", "again", "Turn it out"],
+
+	// Harlem Shuffle
+	["You move it to the left, yeah", "You go for", "yourself", "You move it to the right"],
+	
+	// 5, 6, 7, 8
+	["Wanna make you mine", "Better", "get in line", "5, 6, 7, 8"],
+	["Foot kickin'", "Finger", "clickin'", "Leather slappin'"],
+	["Hip bumpin'", "Music", "thumpin'", "Knee hitchin'"],
 ];
   
 module.exports = quotes;
